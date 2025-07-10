3b5847bac7798f24516b2f4fe42fc3e1a3375bb7
AimanTahrim <m.aimansyafiqtahrim@gmail.com>
2025-07-10 10:34:27 +0800

    Add Cypress test cases

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners if needed
     },
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
   },
 });
