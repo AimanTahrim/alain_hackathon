df276327132c59c39318f520e5d0961b7fc412de
AimanTahrim <m.aimansyafiqtahrim@gmail.com>
2025-07-10 15:53:33 +0800

    testing1

--- cypress/e2e/aa.cy.js
+++ cypress/e2e/aa.cy.js
@@ -1,6 +1,4 @@
-describe("Simple Smoke Test", () => {
-  it("should visit Google and check title", () => {
-    cy.visit("https://www.google.com");
-    cy.title().should("include", "Google");
-  });
+it("should visit Google and check title1", () => {
+  cy.visit("https://www.google.com");
+  cy.title().should("include", "Firefox");
 });
