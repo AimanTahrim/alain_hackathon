35406b508e6cb3658a125dae61b2a7db9ee16bbc
AimanTahrim <m.aimansyafiqtahrim@gmail.com>
2025-07-10 23:44:32 +0800

    Test Report Load Faster2

--- cypress/e2e/bb.cy.js
+++ cypress/e2e/bb.cy.js
@@ -1,5 +1,5 @@
 it("TC004", () => {
   cy.login();
-  cy.url().should("include", "/items");
+  cy.url().should("include", "/dashboard");
   cy.contains("Select Your Items", { timeout: 10000 }).should("be.visible");
 });
